040dbf3c56be92a8d5fbf7fbbf9c5848fca02866
ivanockupy <ivan@ockupy.com>
2017-08-23 14:32:56 -0400

    setup basic nav layout

--- imports/ui/components/Header/header.jsx
+++ imports/ui/components/Header/header.jsx
@@ -6,7 +6,11 @@ export default class Header extends Component {
     super();
 
   }
+
+
   componentDidMount(){
+
+
     let showNav = false;
     $('.activate-nav').on('click', function(){
       showNav = !showNav;
@@ -33,16 +37,21 @@ export default class Header extends Component {
       <header>
         <div className="activate-nav">MENU</div>
         <nav className="ib-nav">
-          
+
           <div className="passing-blue">
             <div className="overlay-blue"></div>
             <div className="nav-menu">
+              <div className="nav-textbox">
+                <p><span className="highlight-text">Home</span> is for the experience,
+                <span className="highlight-text"> About</span> is for satisfying your curiosity and
+                <span className="highlight-text"> Projects</span> are for the ones who are interested!</p>
+              </div>
               <ul className="nav-list">
-                <li class="nav-list-item"><a href="" className="nav-link">Link 1</a></li>
-                <li class="nav-list-item"><a href="" className="nav-link">Link 1</a></li>
-                <li class="nav-list-item"><a href="" className="nav-link">Link 1</a></li>
-                <li class="nav-list-item"><a href="" className="nav-link">Link 1</a></li>
+                <li className="nav-list-item"><a href="" className="nav-link"><label>01</label>Home</a></li>
+                <li className="nav-list-item"><a href="" className="nav-link"><label>02</label>About</a></li>
+                <li className="nav-list-item"><a href="" className="nav-link"><label>03</label>Projects</a></li>
               </ul>
+              <p className="nav-copy">Thanks for visiting my development</p>
             </div>
           </div>
 
